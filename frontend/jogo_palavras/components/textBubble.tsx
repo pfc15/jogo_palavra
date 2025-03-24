f06aa43739a00d81118b1052678ad3c90ce8aaba
pedro cruz <pedrofcrux@gmail.com>
2025-03-24 15:02:41 -0300

    bolh de recebido e enviado

--- frontend/jogo_palavras/components/textBubble.tsx
+++ frontend/jogo_palavras/components/textBubble.tsx
@@ -6,27 +6,59 @@ interface TextBubbleProps {
     mensagem: string,
     send: boolean,
     hora: Date,
+    received: boolean
   }
   
 //  
-const TextBubble: React.FC<TextBubbleProps> = ({ author, mensagem, send, hora}) => {
+const TextBubble: React.FC<TextBubbleProps> = ({ author, mensagem, send, hora, received}) => {
+    const styles = received? styles_received: styles_send
     return (
         <View style={styles.container}>
-            <Text> {author}</Text>
-            <Text> {mensagem}</Text>
-            {send?<Text>enviou!</Text>:null}
-            <Text>{hora.toISOString()}</Text>
+            <Text style={styles.author}> {author}</Text>
+            <Text style={styles.mensagem}> {mensagem}</Text>
+            <Text style={styles.data}>{hora.toISOString()}{send?"  enviou!":null}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const styles_received = StyleSheet.create({
     container: {
-        width: '40%',
+        width: '45%',
         backgroundColor: '#70A3CC',
-        margin: 5,
+        margin: 10,
         borderRadius: '2%',
     },
+    author: {
+        fontWeight: 'bold',
+        alignSelf: 'auto'
+    },
+    mensagem: {
+        marginLeft: 10,
+    },
+    data: {
+        color: 'grey',
+        alignSelf: 'flex-end'
+    }
+})
+const styles_send = StyleSheet.create({
+    container: {
+        width: '45%',
+        backgroundColor: '#90EE90',
+        margin: 10,
+        borderRadius: '2%',
+        alignSelf: 'flex-end'
+    },
+    author: {
+        fontWeight: 'bold',
+        alignSelf: 'auto'
+    },
+    mensagem: {
+        marginLeft: 10,
+    },
+    data: {
+        color: 'grey',
+        alignSelf: 'flex-end'
+    }
 })
 
 export default TextBubble;
