d42d71f16cc6737824221160124db29dc7c5a78a
pedro cruz <pedrofcrux@gmail.com>
2025-04-03 21:08:35 -0300

    modificações no global do ip

--- frontend/jogo_palavras/connection/globals.ts
+++ frontend/jogo_palavras/connection/globals.ts
@@ -1,8 +1,8 @@
 import connection from "./connection"
 
-const global_network: {network: InstanceType<typeof connection> | null, ip: string} = {
+var global_network: {network: InstanceType<typeof connection> | null, ip: string} = {
     network: null,
-    ip: "localhost:8000"
+    ip: "192.168.0.193:8000"
 }
 
 export default global_network
